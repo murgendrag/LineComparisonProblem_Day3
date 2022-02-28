51a2e4c7b5c47560ec246cf0219a2efa63b8def3
murgendrag <murgendra67@gmail.com>
2022-02-28 20:50:02 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    /// <summary>
+    /// greeting message
+    /// </summary>
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Computation Program ");
+        }
+    }
+}
